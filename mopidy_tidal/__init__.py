9c6c85cbef0308348f474e210de7181401e6c25a
John Maximilian <2e0byo@gmail.com>
2022-07-20 21:04:48 +0200

    chore: blacken.

--- mopidy_tidal/__init__.py
+++ mopidy_tidal/__init__.py
@@ -6,8 +6,7 @@ import sys
 
 from mopidy import config, ext
 
-
-__version__ = '0.2.8'
+__version__ = "0.2.8"
 
 # TODO: If you need to log, use loggers named after the current Python module
 logger = logging.getLogger(__name__)
@@ -15,21 +14,22 @@ logger = logging.getLogger(__name__)
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 
+
 class Extension(ext.Extension):
 
-    dist_name = 'Mopidy-Tidal'
-    ext_name = 'tidal'
+    dist_name = "Mopidy-Tidal"
+    ext_name = "tidal"
     version = __version__
 
     def get_default_config(self):
-        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        conf_file = os.path.join(os.path.dirname(__file__), "ext.conf")
         return config.read(conf_file)
 
     def get_config_schema(self):
         schema = super(Extension, self).get_config_schema()
-        schema['quality'] = config.String(choices=["LOSSLESS", "HIGH", "LOW"])
-        schema['client_id'] = config.String(optional=True)
-        schema['client_secret'] = config.String(optional=True)
+        schema["quality"] = config.String(choices=["LOSSLESS", "HIGH", "LOW"])
+        schema["client_id"] = config.String(optional=True)
+        schema["client_secret"] = config.String(optional=True)
         schema["track_cache_size"] = config.Integer(optional=True)
         schema["track_cache_enabled"] = config.Boolean(optional=True)
         schema["track_cache_timeout"] = config.Integer(optional=True)
@@ -37,4 +37,5 @@ class Extension(ext.Extension):
 
     def setup(self, registry):
         from .backend import TidalBackend
-        registry.add('backend', TidalBackend)
+
+        registry.add("backend", TidalBackend)
